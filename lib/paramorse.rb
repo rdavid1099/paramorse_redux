f9b799092c63eb4ffa8c0bb61c939d239715655a
Ryan Workman <rdavid1099@gmail.com>
2016-07-12 15:53:44 -0600

    Added and test file decoder.  Working on decoding/ encoding bugs regarding to spec chars

--- lib/paramorse.rb
+++ lib/paramorse.rb
@@ -175,6 +175,39 @@ module ParaMorse
   end
 
   class FileDecoder
-    
+    attr_reader :encoded_filename,
+                :plain_filename
+    def initialize
+      @decoder = Decoder.new
+    end
+
+    def decode(encode, plain)
+      @encoded_filename = check_for_txt(encode)
+      @plain_filename = check_for_txt(plain)
+      file_validated ? write_file : "File does not exist"
+    end
+
+    def file_validated
+      File.exist?(@encoded_filename)
+    end
+
+    def check_for_txt(filename)
+      filename = "#{filename}.txt" unless filename =~ /.txt/
+      filename
+    end
+
+    def encoded_file_contents
+      File.read(@encoded_filename)
+    end
+
+    def decode_file_contents
+      @decoder.decode(encoded_file_contents)
+    end
+
+    def write_file
+      file = File.new(@plain_filename, 'w')
+      file.write(decode_file_contents)
+      file.close
+    end
   end
 end
--- test/file_decoder_test.rb
+++ test/file_decoder_test.rb
@@ -10,6 +10,31 @@ class TestFileDecoder < Minitest::Test
     assert_instance_of ParaMorse::FileDecoder, @f
   end
   def test_file_decoder_finds_files_passed_into_it
-    assert @f('test_encode.txt','test.txt')
+    @f.decode('test_encode.txt','test.txt')
+    assert_equal 'test_encode.txt', @f.encoded_filename
+    assert_equal 'test.txt', @f.plain_filename
+  end
+  def test_file_decoder_adds_txt_to_filenames
+    @f.decode('test_encode','test')
+    assert_equal 'test_encode.txt', @f.encoded_filename
+    assert_equal 'test.txt', @f.plain_filename
+  end
+  def test_file_decoder_confirms_encoded_file_existance
+    assert_equal "File does not exist", @f.decode('no_file_name', 'test')
+  end
+  def test_file_decoder_reads_encoded_file
+    @f.decode('test_encode', 'test')
+    assert_equal '10101010001010000000111010101', @f.encoded_file_contents
+  end
+  def test_file_decoder_decodes_txt_file
+    @f.decode('test_encode', 'test')
+    assert_equal 'hi b', @f.decode_file_contents
+  end
+  def test_file_decoder_saves_decoded_text_to_filename
+    @f.decode('test_encode', 'this_test')
+    assert_equal 'hi b', File.read('this_test.txt')
+  end
+  def test_file_decoder_decodes_special_characters
+    @f.decode('spec_chars_encoded', 'spec_chars_decode')
   end
 end
