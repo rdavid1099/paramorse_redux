c6076d3d2e12dc29138a62d0ea904ab65defee44
Ryan Workman <rdavid1099@gmail.com>
2016-07-12 17:22:38 -0600

    Created StreamEncoder class and functionality

--- lib/paramorse.rb
+++ lib/paramorse.rb
@@ -152,6 +152,34 @@ module ParaMorse
     end
   end
 
+  class StreamEncoder
+    attr_reader :current_queue
+
+    def initialize
+      @current_queue = Queue.new
+    end
+
+    def receive(input)
+      @current_queue.push(input)
+    end
+
+    def stream_queue
+      chars_to_be_encoded = String.new
+      until @current_queue.count == 0
+        chars_to_be_encoded += @current_queue.pop
+      end
+      chars_to_be_encoded
+    end
+
+    def encode_one_letter
+      Encoder.new.encode(@current_queue.pop)
+    end
+
+    def encode_entire_stream
+      Encoder.new.encode(stream_queue)
+    end
+  end
+
   class FileEncoder
     attr_reader :plain_filename,
                 :encode_filename
---
+++ test/stream_encoder_test.rb
@@ -0,0 +1,45 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/paramorse'
+
+class TestStreamEncoder < Minitest::Test
+  def setup
+    @s = ParaMorse::StreamEncoder.new
+  end
+  def test_it_exists
+    assert_instance_of ParaMorse::StreamEncoder, @s
+  end
+  def test_stream_has_a_queue
+    assert @s.current_queue
+  end
+  def test_stream_can_receive_letters
+    assert_respond_to @s, :receive
+  end
+  def test_stream_puts_input_into_queue
+    @s.receive('e')
+    assert_equal 1, @s.current_queue.count
+  end
+  def test_stream_encoder_encodes_elements_in_queue
+    @s.receive('e')
+    assert_equal '1', @s.encode_one_letter
+  end
+  def test_stream_can_encode_one_word
+    @s.receive('b')
+    @s.receive('e')
+    assert_equal '1110101010001', @s.encode_entire_stream
+  end
+  def test_stream_can_encode_statement
+    @s.receive('h')
+    @s.receive('e')
+    @s.receive('l')
+    @s.receive('l')
+    @s.receive('o')
+    @s.receive(' ')
+    @s.receive('w')
+    @s.receive('o')
+    @s.receive('r')
+    @s.receive('l')
+    @s.receive('d')
+    assert_equal '101010100010001011101010001011101010001110111011100000001011101110001110111011100010111010001011101010001110101', @s.encode_entire_stream
+  end
+end
