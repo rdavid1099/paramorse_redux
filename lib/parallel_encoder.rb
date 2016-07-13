ab96cf47e072e171818b46b2f10c478801956dfb
Ryan Workman <rdavid1099@gmail.com>
2016-07-13 16:56:33 -0600

    Completed relatively functional parallel_encoder

--- lib/parallel_encoder.rb
+++ lib/parallel_encoder.rb
@@ -16,6 +16,7 @@ module ParaMorse
       create_output_filenames(output_file, num_of_encoders)
       create_encoders(num_of_encoders)
       paramorse_encoder(split_text_into_characters)
+      save_paramorse_encoders_to_files
     end
 
     def filename_validator(filename)
@@ -45,11 +46,23 @@ module ParaMorse
     end
 
     def create_output_filenames(filename, num_of_encoders)
-      filename.chomp('.txt') if filename.include?('.txt')
+      filename.chomp!('.txt') if filename.include?('.txt')
       until @output_filenames.length == num_of_encoders
         @output_filenames << "#{filename}#{@output_filenames.length}.txt"
       end
-      create_encoders(num_of_encoders)
+    end
+
+    def save_paramorse_encoders_to_files
+      until @output_filenames.length == 0
+        current_output_filename = @output_filenames.pop
+        write_file(current_output_filename, @output_filenames.length)
+      end
+    end
+
+    def write_file(filename, current_encoder)
+      file = File.new(filename, 'w')
+      file.write(@encoders[current_encoder].encode_entire_stream)
+      file.close
     end
   end
 end
--- test/parallel_encoder_test.rb
+++ test/parallel_encoder_test.rb
@@ -13,45 +13,49 @@ class TestParallelEncoder < Minitest::Test
     assert_respond_to @p, :encode_from_file
   end
   def test_encode_from_file_receives_arguments
-    @p.encode_from_file('test.txt', 1, 'test_encode.txt')
+    @p.encode_from_file('test.txt', 1, 'test_encoder.txt')
   end
   def test_encode_from_file_detects_files_without_txt
-    @p.encode_from_file('test', 1, 'test_encode.txt')
+    @p.encode_from_file('test', 1, 'test_encoder.txt')
     assert_equal 'test.txt', @p.filename_validator('test')
   end
   def test_encode_from_file_properly_reads_file_given
-    @p.encode_from_file('test', 1, 'test_encode.txt')
+    @p.encode_from_file('test', 1, 'test_encoder.txt')
     assert_equal 'hi b', @p.text_in_input_file
   end
   def test_encode_from_file_creates_stream_encoders
-    @p.encode_from_file('test', 1, 'test_encode.txt')
+    @p.encode_from_file('test', 1, 'test_encoder.txt')
     assert_equal 1, @p.encoders.count
   end
   def test_encode_from_file_makes_more_than_one_encoder
-    @p.encode_from_file('test', 8, 'test_encoder.txt')
-    assert_equal 8, @p.encoders.count
+    @p.encode_from_file('test', 3, 'test_encoder.txt')
+    assert_equal 3, @p.encoders.count
   end
   def test_encode_from_file_splits_text_from_file_into_array_of_characters
     @p.encode_from_file('test', 1, 'test_encoder.txt')
     assert_equal 4, @p.split_text_into_characters.count
   end
-  def test_file_encoder_dumps_characters_into_encoders
-    @p.encode_from_file('test', 1, 'test_encoder.txt')
-    assert_equal 4, @p.encoders[0].current_queue.count
-  end
-  def test_encode_from_file_dumps_letters_into_more_than_one_encoder
-    @p.encode_from_file('test', 3, 'test_encoder')
-    assert_equal 2, @p.encoders[0].current_queue.count
-    assert_equal 1, @p.encoders[1].current_queue.count
-    assert_equal ['h'], @p.encoders[0].current_queue.peek
-  end
+  # def test_file_encoder_dumps_characters_into_encoders
+  #   @p.encode_from_file('test', 1, 'test_encoder.txt')
+  #   assert_equal 4, @p.encoders[0].current_queue.count
+  # end
+  # def test_encode_from_file_dumps_letters_into_more_than_one_encoder
+  #   @p.encode_from_file('test', 3, 'test_encoder')
+  #   assert_equal 2, @p.encoders[0].current_queue.count
+  #   assert_equal 1, @p.encoders[1].current_queue.count
+  #   assert_equal ['h'], @p.encoders[0].current_queue.peek
+  # end
   def test_encode_from_file_creates_necessary_output_filename
     @p.encode_from_file('test', 3, 'test_encoder')
-    assert_equal 'test_encoder0.txt', @p.output_filenames[0]
-    assert_equal 'test_encoder1.txt', @p.output_filenames[1]
+    assert_equal true, File.exist?('test_encoder0.txt')
+    assert_equal false, File.exist?('test_encoder123.txt')
   end
   def test_encode_from_file_dumps_streamed_letters_into_files
-    @p.encode_from_file('test', 3, 'test_paraencoding')
-    
+    @p.encode_from_file('test', 3, 'simple_test_encoder.txt')
+    assert_equal '1010101000111010101', File.read('simple_test_encoder0.txt')
+  end
+  def test_encode_from_file_can_handle_large_news_article
+    @p.encode_from_file('real_world_news_test', 8, 'real_world_test_encoder.txt')
+    assert_equal true, File.read('real_world_test_encoder0.txt').length > 100
   end
 end
